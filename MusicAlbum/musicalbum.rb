e92a0e13c4fb1e0f225472152d9affd868430f0d
Farida-Hamid <faridaaddala@gmail.com>
2022-11-29 12:00:36 +0300

    Build the MusicAlbum class

---
+++ MusicAlbum/musicalbum.rb
@@ -0,0 +1,18 @@
+require_relative '../item'
+
+class MusicAlum < Item
+  attr_reader :on_spotify
+
+  def initialize (on_spotify, published_date)
+    super(published_date, false)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?()
+    super() && @on_spotify
+  end
+end
+
+## test intance
+alb = MusicAlum.new(true, 3)
+puts alb.can_be_archived?
