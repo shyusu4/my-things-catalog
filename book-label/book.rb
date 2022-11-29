835a3d39936a4e39102e700c21ca1f6cb2c65546
shyusu4 <shyusupova4@gmail.com>
2022-11-29 13:37:15 +0500

    Initialize book class

--- book-label/book.rb
+++ book-label/book.rb
@@ -1,4 +1,15 @@
 require_relative '../item'
 
-class Book
+class Book < Item
+    attr_reader :published_date, :publisher, :cover_state
+  
+    def initialize(published_date, publisher, cover_state, archived = false)
+      super(published_date, archived)
+      @publisher = publisher
+      @cover_state = cover_state
+    end
+  
+    def can_be_archived?
+      super || cover_state == 'bad'
+    end
 end
