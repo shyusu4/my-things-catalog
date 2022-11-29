ee219f2bb1e3d468076114c147a2081a7e62c44b
Farida-Hamid <faridaaddala@gmail.com>
2022-11-29 13:56:44 +0300

    Build the Genere class

---
+++ Genere/genere.rb
@@ -0,0 +1,19 @@
+class Genere
+  attr_reader :name, :id
+
+  def initialize(name)
+    @id = Random.rand(100)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    items.genre = self
+  end
+end
+
+
+# For testing run `ruby ./Genere/genere.rb`
+genere = Genere.new('comedy')
+puts genere.id, genere.name
