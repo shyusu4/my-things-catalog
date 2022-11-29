de6ef3e35d7fec5958804dfe1481c0537f764527
shyusu4 <shyusupova4@gmail.com>
2022-11-29 13:57:55 +0500

    Update super

--- book-label/book.rb
+++ book-label/book.rb
@@ -1,15 +1,15 @@
 require_relative '../item'
 
 class Book < Item
-    attr_reader :published_date, :publisher, :cover_state
-  
-    def initialize(published_date, publisher, cover_state, archived = false)
-      super(published_date, archived)
-      @publisher = publisher
-      @cover_state = cover_state
-    end
+  attr_reader :published_date, :publisher, :cover_state
 
-    def can_be_archived?
-        super || cover_state == 'bad'
-      end
+  def initialize(published_date, publisher, cover_state)
+    super(published_date, false)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || cover_state == 'bad'
+  end
 end
