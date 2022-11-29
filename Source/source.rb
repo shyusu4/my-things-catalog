2d5c8037b60d749df5fc663ce76408de40224f40
tarikbouari <tarikbouari@gmail.com>
2022-11-29 13:01:43 +0000

    Update source class

---
+++ Source/source.rb
@@ -0,0 +1,15 @@
+require_relative './item'
+
+class Source
+  attr_accessor :name, :items, :id
+  def initialize(name)
+    @id = Random.rand(1.9999)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.source = self
+  end
+end
