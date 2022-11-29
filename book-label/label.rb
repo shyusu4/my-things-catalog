a7d0ffbae1a03499c6f44cd814eb2c6760e40538
shyusu4 <shyusupova4@gmail.com>
2022-11-29 13:57:43 +0500

    Initialize label class and add add_item method

--- book-label/label.rb
+++ book-label/label.rb
@@ -1,2 +1,15 @@
 class Label
+  attr_reader :id, :items, :title, :color
+
+  def initialize(title, color)
+    @id = Random.rand(1000)
+    @items = []
+    @title = title
+    @color = color
+  end
+
+  def add_item(item)
+    @items << item
+    item.label = self
+  end
 end
