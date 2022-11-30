8e047c14b5ef536ee55e764b8fe24a0e0414abe1
Farida-Hamid <faridaaddala@gmail.com>
2022-11-30 18:48:53 +0300

    Fix linter errors

--- spec/musicalbum_spec.rb
+++ spec/musicalbum_spec.rb
@@ -1,17 +1,17 @@
 require_relative '../MusicAlbum/musicalbum'
 
-describe "Test the MusicAlbum class:" do
-  let(:album) {MusicAlbum.new(true, "20-02-2020")}
+describe 'Test the MusicAlbum class:' do
+  let(:album) { MusicAlbum.new(true, '20-02-2020') }
 
-  it "Check the on_spotify property:" do
+  it 'Check the on_spotify property:' do
     expect(album.on_spotify).to be(true)
   end
 
-  it "Check the publish_date property:" do
-    expect(album.published_date).to eql "20-02-2020"
+  it 'Check the publish_date property:' do
+    expect(album.published_date).to eql '20-02-2020'
   end
 
-  it "Check the can_be_archived methode:" do
+  it 'Check the can_be_archived methode:' do
     expect(album.can_be_archived?).to be(false)
   end
 end
