e9a317f08dc120a0b8e10d152b596ff58601de26
Rikstone <tarikbouari@gmail.com>
2022-11-30 11:10:54 +0000

    Resolve linters error

--- Movies/movie_rspec.rb
+++ Movies/movie_rspec.rb
@@ -1,7 +1,7 @@
 require_relative './movie'
 describe Movie do
   it 'should test if Movie can be achived' do
-    movie = Movie.new('2012/02/08', silet = true)
+    movie = Movie.new('2012/02/08', true)
     expect(movie.published_date).to eq '2012/02/08'
     expect(movie.silet).to eq true
     expect(movie).to respond_to(:can_be_archived?)
