69f6da810973d28cd54042aa4c32b61f54e1adb4
Shakhrizoda Yusupova <shyusupova4@gmail.com>
2022-11-30 19:53:39 +0500

    Revert "2: Book and Label classes"

--- spec/source_rspec.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../Source/source'
-
-describe Source do
-  it 'Should test name as a property' do
-    source = Source.new('Japan')
-    expect(source.name).to eq 'Japan'
-  end
-end
