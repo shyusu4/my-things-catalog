4abf16f5bf498cbdee2003be845151bb006584d5
tarikbouari <tarikbouari@gmail.com>
2022-11-30 10:22:18 +0000

    Update Unit  test for movie class

--- Movies/movie_rspec.rb
+++ Movies/movie_rspec.rb
@@ -1,8 +1,16 @@
 require_relative './movie'
 
 describe Movie do
+  # it 'should test if Movie can be achived' do
+  #   movie = Movie.new('2012/02/08', silet: true)
+  #   expect(movie.silet).to eq true
+  #   expect(movie.published_date).to eq '2012/02/08'
+  #   expect(movie).to respond_to(:can_be_archived?)
+  # end
   it 'should test if Movie can be achived' do
-    movie = Movie.new('2012/02/08', silet: true)
-    expect(movie.can_be_archived?).to eq true
+    movie = Movie.new('2012/02/08', silet= true)
+    expect(movie.published_date).to eq '2012/02/08'
+    expect(movie.silet).to eq true
+    expect(movie).to respond_to(:can_be_archived?)
   end
 end
