89087a00850906e4c69755ebf6f6f188faa05fef
Farida-Hamid <faridaaddala@gmail.com>
2022-12-01 11:51:10 +0300

    Fix source meddage

--- Source/source_method.rb
+++ Source/source_method.rb
@@ -1,7 +1,7 @@
 module SourcesFunctions
   def list_sources(sources)
     sources.each_with_index do |source, index|
-      puts "#{index + 1} - #{source.name}"
+      puts "#{index + 1} - Source: #{source.name}"
     end
   end
 end
