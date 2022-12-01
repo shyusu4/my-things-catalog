3fa8dfe5e9b22740fbec618341c64559a003fad2
Farida-Hamid <faridaaddala@gmail.com>
2022-12-01 11:32:58 +0300

    Merge schema

--- Database/schema.sql
+++ Database/schema.sql
@@ -15,14 +15,14 @@ CREATE TABLE games(
     multiplayer varchar(255),
     last_palayed_at DATE,
     publish_date Date,
-    archived Boolean,
-)
+    archived Boolean
+);
 
 CREATE TABLE authors(
     id serial PRIMARY KEY,
     first_name varchar(255),
-    last_name varchar(255),
-)
+    last_name varchar(255)
+);
 
 CREATE TABLE music_album(
     on_spotify Boolean,
@@ -32,4 +32,15 @@ CREATE TABLE music_album(
 CREATE TABLE genere(
     id serial PRIMARY KEY,
     name varchar(25)
+);
+
+CREATE TABLE movie (
+    id BIGSERIAL PRIMARY KEY,
+    silet boolean,
+    published_date date
+);
+
+CREATE TABLE source (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255)
 );
